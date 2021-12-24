db2d524ecae24726bacd8a7d30ee75f525059886
Dmitriy Solodukha <dspv@me.com>
2021-12-24 13:21:12 +0200

    Add more outputs: Cluster ARN (#11)

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "cluster_name" {
   value = aws_ecs_cluster.cluster.name
 }
 
+output "cluster_arn" {
+  value = aws_ecs_cluster.cluster.arn
+}
+
 output "cluster_capacity_provider" {
   value = aws_ecs_capacity_provider.cluster_cp.name
 }
