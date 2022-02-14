397f98ae75de96c5a4bd7de7b02c71943452909b
Dmitriy Solodukha <dmitriy@solodukha.com>
2022-02-14 15:24:44 +0200

    Revert "Some initial improvements (#12)" (#13)
    This reverts commit 3012311f00b749e9f75f01f78fec6ec4ccf535dc.

--- user-data.sh
+++ user-data.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+sudo yum -y install mdadm
+sudo mdadm --create --verbose /dev/md0 --level=0 --name=MY_RAID0 --raid-devices=3 /dev/sdc /dev/sdd /dev/sde
+sudo mkfs.ext4 -L MY_RAID0 /dev/md0
+sudo mkdir -p /datadir/tmp
+sudo mount LABEL=MY_RAID0 /datadir
+
 # ECS config
 echo ECS_CLUSTER="${cluster_name}" >> /etc/ecs/ecs.config
 echo ECS_ENABLE_CONTAINER_METADATA=true >> /etc/ecs/ecs.config
+
 start ecs
+
 echo "Done"
